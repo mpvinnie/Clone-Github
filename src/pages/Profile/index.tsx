455593dd2e38fcb30be2d7bff76edfe0e38e7585
mpvinnie <vinniemalafaia@hotmail.com>
2020-08-28 19:45:09 -0400

    Repositorie Title

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -6,7 +6,9 @@ import {
   LeftSide,
   RightSide,
   Repos,
-  CalendarHeading
+  CalendarHeading,
+  RepoIcon,
+  Tab
 } from './styles';
 
 import ProfileData from '../../components/ProfileData'
@@ -14,8 +16,24 @@ import RepoCard from '../../components/RepoCard'
 import RandomCalendar from '../../components/RandomCalendar'
 
 const Profile: React.FC = () => {
+  const TabContent = () => (
+    <div className="content">
+      <RepoIcon />
+      <span className="label">Repositories</span>
+      <span className="number">23</span>
+    </div>
+  )
+
   return (
     <Container>
+      <Tab className="desktop">
+        <div className="wrapper">
+          <span className="offset" />
+          <TabContent />
+        </div>
+
+        <span className="line" />
+      </Tab>
       <Main>
         <LeftSide>
           <ProfileData 
@@ -33,6 +51,11 @@ const Profile: React.FC = () => {
         </LeftSide>
 
         <RightSide>
+          <Tab className="mobile">
+            <TabContent />
+            <span className="line" />
+          </Tab>
+
           <Repos>
             <h2>Random Repos</h2>
 
