f7299acd817f159895b05b4c341a0bea57a05fc9
mpvinnie <vinniemalafaia@hotmail.com>
2020-08-27 23:29:01 -0400

    Profile Side

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+import ProfileData from '../../components/ProfileData'
 
 const Profile: React.FC = () => {
   return (
     <Container>
-      <h1>Profile</h1>
+      <Main>
+        <LeftSide>
+          <ProfileData 
+            username={'mpvinnie'}
+            name={'Vinicius Malafaia'}
+            avatarUrl={'https://avatars1.githubusercontent.com/u/61069632?v=4'}
+            followers={4}
+            following={3}
+            company={'MP Vinnie'}
+            location={'Manaus, Brazil'}
+            email={'vinniemalafaia@hotmail.com'}
+            blog={'https://www.linkedin.com/in/vinicius-malafaia-2b8a50196'}
+          />
+            
+        </LeftSide>
+
+        <RightSide>
+
+        </RightSide>
+      </Main>
     </Container>
   );
 };
