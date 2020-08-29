a5e2b92879f1838fe55e08ca886fa5212cda615a
mpvinnie <vinniemalafaia@hotmail.com>
2020-08-28 20:22:01 -0400

    Repo Route

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,11 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
-import { Container } from './styles';
+import {
+  Container,
+  BreadCrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon
+} from './styles';
 
 const Repo: React.FC = () => {
   return (
     <Container>
-      <h1>Repo</h1>
+      <BreadCrumb>
+        <RepoIcon />
+        <Link className="username" to={'/mpvinnie'}>
+          mpvinnie
+        </Link>
+
+        <span>/</span>
+
+        <Link className="reponame" to={'/mpvinnie/Proffy'}>
+          Proffy
+        </Link>
+      </BreadCrumb>
+
+      <p>Plataforma online de estudos entre professores e alunos.</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={'https://github.com/mpvinnie/Proffy'}>
+        <GithubIcon />
+        <span>View on Github</span>
+      </LinkButton>
     </Container>
   );
 };
